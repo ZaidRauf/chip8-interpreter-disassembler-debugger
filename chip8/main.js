3389e478c8d76f93ac1616985117894e1761fdd4
zrauf <zrauf@sfu.ca>
2021-05-20 22:44:22 -0700

    Move chip8 files into folder

--- chip8/main.js
+++ chip8/main.js
@@ -1,9 +1,19 @@
-canvas_init(1024)
+"use strict"
+
 var c8 = new chip8();
+
+canvas_init(1024)
+
 read_chip8_file_init(c8)
+
 initExecutionButtons(c8)
+
 initSelectDropdown(c8)
+
 initColorPicker(c8)
+
 initRegisterInfo(c8)
+
 initRunOnLoad()
+
 initDebuggerInputs(c8)
